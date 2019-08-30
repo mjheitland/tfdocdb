3b1be82a17776a05e556242c66d0107e4d90ff0d
Michael Heitland <heitlm@amazon.de>
2019-08-30 15:50:19 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+#--- root/main.tf ---
+provider "aws" {
+}
+
+# deploy networking resources
+module "networking" {
+  source        = "./networking"
+  
+  project_name  = var.project_name
+  vpc_cidr      = var.vpc_cidr
+  subpub_cidrs  = var.subpub_cidrs
+  subprv_cidrs  = var.subprv_cidrs
+  access_ip     = var.access_ip
+  service_ports = var.service_ports
+}
+
+# Deploy Compute Resources
+module "compute" {
+  source          = "./compute"
+  
+  project_name    = var.project_name
+  key_name        = var.key_name
+  public_key_path = var.public_key_path
+  instance_type   = var.instance_type
+  subpub_ids      = module.networking.subpub_ids
+  sg_id           = module.networking.sg_id
+}
